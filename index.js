fb770356cbe148752734f321d8dbac103d8b1c3d
Steven <79530837+STMason27@users.noreply.github.com>
2021-09-07 19:45:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const validatePassword = require('./passwordvalidation'); //import the function
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
 
-// Your code goes here!
+getPassword();
+
+
+function getPassword(){
+
+    rl.question('What is your Enter a password to validate (or :q to quit)' , (pass) => {
+
+        if(validatePassword(pass) == true){
+            console.log("the password is valid");
+            return getPassword();
+        }
+        else if(validatePassword(pass) == false && pass == 'q'){
+            rl.close();
+        }
+        else if(validatePassword(pass) == false){
+            console.log("invalid pasword");
+            return getPassword();
+        }  
+
+        });
+
+}
